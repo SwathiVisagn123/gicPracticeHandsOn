df8764073376905be6c8892b47b60edd035d9707
swathikavisagn <swathika.visagn@gmail.com>
2021-06-14 23:19:34 +0100

    started off with basics

--- src/main/java/utils/CommonFunctions.java
+++ src/main/java/utils/CommonFunctions.java
@@ -1,4 +1,46 @@
 package utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class CommonFunctions {
+
+    public static WebDriver driver;
+   String filePath = new File ("src/main/resources/testData/urlInfo.properties").getAbsolutePath();
+
+
+    public void openBrowser()
+    {
+        //sets up the chromedriver
+    WebDriverManager.chromedriver().setup();
+        //open chrome browser
+    driver = new ChromeDriver();
+
+    }
+
+    public void closeBrowser()
+    {
+        driver.quit();
+    }
+
+    public String passUrlPath(String key) throws IOException {
+        FileInputStream fileInputStream= new FileInputStream(filePath);
+        Properties properties = new Properties();
+        properties.load(fileInputStream);
+        String value = properties.getProperty(key);
+        return value;
+
+
+    }
+
+
+
+
 }
--- src/test/java/Hooks.java
+++ src/test/java/Hooks.java
@@ -1,2 +1,24 @@
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import utils.CommonFunctions;
+
 public class Hooks {
+
+    CommonFunctions commonFunctions = new CommonFunctions();
+
+    //importing annotation from cucumber
+    @Before
+    public void beforeTest()
+    {
+        commonFunctions.openBrowser();
+    }
+
+//importing annotation from cucumber
+    @After
+    public void  afterTest()
+    {
+        commonFunctions.closeBrowser();
+    }
+
+
 }
